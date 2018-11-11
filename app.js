cfeeb100aef930f5e5b35f36bf16faab5d1a1e64
Mirian <mirianejerenwa@yahoo.com>
2018-11-10 20:38:00 -0500

    updates

--- app.js
+++ app.js
@@ -1,161 +1,67 @@
-$(function() {
+// create an array of strings, 
+// each one related to a stock symbol that interests you. Save it to a variable called `stocksList`
 
-    const render = function (NYTData) {
+var stocksList = ['FB','AMZN', 'MSFT','BLDR', 'SNAP','CNAT'];
+
+// Your app should take the stocks in this array and create buttons in your HTML.
+const createButton = function () {
+  // for loop to go through array length and make buttons for them
+
+for (let i = 0; i < stocksList.length; i++) {
+  const element = stocksList[i];
+  console.log(element);
   
-      // Get from the form the number of results to display
-      // API doesn't have a 'limit' parameter, so we have to do this ourselves
-      const numArticles = $('#article-count').val();
-      const articles = NYTData.response.docs.slice(0, numArticles);
+  // create a variable for buttons
+  const stockBtn = $('<button>');
+  // give the button a class 'createBtn'
+stockBtn.addClass('createBtn');
+// give btn attr
+stockBtn.attr('data-name', element);
+// add a text to the stockBtn, which is the element name
+stockBtn.text(element);
+// append the buttons to html div 'magicBtns'
+$('.magicBtns').append(stockBtn);
+}
+}
+createButton();
+
+
+// When the user clicks on a button, the page should grab the company name, 
+// logo, price, and up to 10 news articles related to the stock from the 
+// iexTrading API and place them on the page.
+
+
+
+
+
+
+
+
+
+
+// $.ajax({
+//   Url:'https://api.nytimes.com/svc/search/v2/articlesearch.json?',
+//   Method:'GET'
+//   })
   
-      articles.forEach(function(article) {
+//   .done(function() { 
+// console.log('response');})
   
-        // Append the article
-        $('#article-section').append(buildArticle(article));
-      });
-    }
-  
-    const buildArticle = function(article) {
-  
-      // Create the  list group to contain the articles and add the article content for each
-      const articleList = $('<ul>');
-  
-      // If the article has a headline, log and append to $articleList
-      const headline = article.headline;
-  
-      const articleListItem = $('<li>')
-  
-      if (headline && headline.main) {
-        console.log(headline.main);
-        articleListItem.append(
-          $('<h3>')
-            .text(headline.main)
-        );
-      }
-  
-      // If the article has a byline, log and append to $articleList
-      const byline = article.byline;
-  
-      if (byline && byline.original) {
-        articleListItem.append($('<h5>').text(byline.original));
-      }
-  
-      // Log section, and append to document if exists
-      const section = article.section_name;
-  
-      if (section) {
-        articleListItem.append($('<h5>').text(`Section: ${section}`));
-      }
-  
-      // Log published date, and append to document if exists
-      const pubDate = article.pub_date;
-  
-      if (pubDate) {
-        articleListItem.append($('<p>').text(formatDate(article.pub_date)));
-      }
-  
-      const articleLink = $('<a>')
-        .attr('href', article.web_url)
-        .attr('target', '_blank');
-  
-      articleLink.append(articleListItem)
-  
-      return articleLink;
-    }
-  
-    const formatDate = function(date) {
-      const months = [
-        'January', 
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
-      ];
-  
-      // Create a new JavaScript Date object from the passed in date
-      const dateObj = new Date(date);
-  
-      // return the date as a string in format 'Month DD, YYYY'
-      return `${months[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`
-    }
-  
-    const search = function (event) {
-  
-      // This line allows us to take advantage of the HTML 'submit' property
-      // This way we can hit enter on the keyboard and it registers the search
-      // (in addition to clicks). Prevents the page from reloading on form submit.
-      event.preventDefault();
-  
-      // Empty the region associated with the articles
-      clear();
-  
-      // Build the query URL for the ajax request to the NYT API
-      const queryURL = buildQueryURL();
-  
-      // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-      // The data then gets passed as an argument to the render function
-      $.ajax({
-        url: queryURL,
-        method: 'GET'
-      }).then(render);
-    }
-  
-    const buildQueryURL = function () {
-    
-      // queryURL is the url we'll use to query the API
-      let queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
-  
-      // Begin building an object to contain our API call's query parameters
-      // Set the API key
-      const queryParams = { 'api-key': 'b9f91d369ff59547cd47b931d8cbc56b:0:74623931' };
-  
-      // Grab text the user typed into the search input, add to the queryParams object
-      queryParams.q = $('#search-term')
-        .val()
-        .trim();
-  
-      // If the user provides a startYear, include it in the queryParams object
-      const startYear = $('#start-year')
-        .val()
-        .trim();
-  
-      if (parseInt(startYear)) {
-        queryParams.begin_date = `${startYear}0101`;
-      }
-  
-      // If the user provides an endYear, include it in the queryParams object
-      const endYear = $('#end-year')
-        .val()
-        .trim();
-  
-      if (parseInt(endYear)) {
-        queryParams.end_date = `${endYear}0101`;
-      }
-  
-      // Logging the URL so we have access to it for troubleshooting
-      console.log('---------------\nURL: ' + queryURL + '\n---------------');
-      console.log(queryURL + $.param(queryParams));
-      return queryURL + $.param(queryParams);
-    }
-  
-    // Function to empty out the articles
-    const clear = function () {
-      $('#article-section').empty();
-    }
-  
-    // CLICK HANDLERS
-    // ==========================================================
-  
-    // .on('click') function associated with the Search Button
-    $('#run-search').on('click', search);
-  
-    //  .on('click') function associated with the clear button
-    $('#clear-all').on('click', clear);
-  });
-  
+//   .fail (function () {
+// console.log(`say what ever you want if you can't get request`);
+//   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /ref-data/symbols
